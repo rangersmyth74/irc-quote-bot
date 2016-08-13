d25503f0420774d7f52b7ded757323c0ec81cd0c
Ed Chen <edychen@gmail.com>
2016-08-13 16:01:06 -0400

    Working random quote implementation with backed datastore

--- bin/bot.js
+++ bin/bot.js
@@ -2,7 +2,8 @@
 'use strict';
 
 var argv = require('yargs')
-    .usage('Usage: quote-bot -c <path to config>')
+    .usage('Usage: quote-bot -c <config_path>')
+    .command('init <channel> <quotes_path>', 'Initialize with quotes from a channel')
     .config('config')
     .default('config', 'config.json')
     .alias('c', 'config')
@@ -11,6 +12,12 @@ var argv = require('yargs')
     .alias('h', 'help')
     .argv
 
+var QuoteStore = require('../lib/quote-store.js');
 var IrcController = require('../lib/irc-controller.js');
 
-var controller = new IrcController(argv);
+var quotes = new QuoteStore(() => {
+    if (argv.channel && argv.quotes_path) {
+        quotes.init('#' + argv.channel, argv.quotes_path);
+    }
+    var controller = new IrcController(quotes, argv);
+});
