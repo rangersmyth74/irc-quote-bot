9af4432d1a10e4e4986787c126f9428b062a98a7
Ed Chen <edychen@gmail.com>
2016-08-13 19:38:06 -0400

    Add logger and constify

--- bin/bot.js
+++ bin/bot.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-var argv = require('yargs')
+const argv = require('yargs')
     .usage('Usage: quote-bot -c <config_path>')
     .command('init <channel> <quotes_path>', 'Initialize with quotes from a channel')
     .config('config')
@@ -12,12 +12,15 @@ var argv = require('yargs')
     .alias('h', 'help')
     .argv;
 
-var QuoteStore = require('../lib/quote-store.js');
-var IrcController = require('../lib/irc-controller.js');
+const logger = require('winston');
+const QuoteStore = require('../lib/quote-store.js');
+const IrcController = require('../lib/irc-controller.js');
 
-var quotes = new QuoteStore(() => {
+var quotes = new QuoteStore(logger, () => {
     if (argv.channel && argv.quotes_path) {
         quotes.init('#' + argv.channel, argv.quotes_path);
+        quotes.close(() => process.exit());
+    } else {
+        new IrcController(logger, quotes, argv);
     }
-    new IrcController(quotes, argv);
 });
